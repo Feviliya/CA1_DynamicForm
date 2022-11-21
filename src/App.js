d74d91d2e758de6267b87f70dbae1b04fbd810a6
Feviliya <93245596+Feviliya@users.noreply.github.com>
2022-11-21 11:51:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { Uibutton,Numbox,Textbox,Bradio,DropdownM,DropdownC,DropdownEmp,DropdownEdu,DropdownR} from './mbutton';
+import {Typography,Link} from '@mui/material';
+import './App.css';
+import {useState} from 'react';
+const initialVal={
+  name:'',
+  gender:'male',
+  marital:'',
+  caste:'',
+  religion:'',
+  eduqual:'',
+  emp:'false',
+  income:0,
+}
+const App=()=>{
+  const [values,setValues]= useState(initialVal)
+  return (   
+    <form>
+      <div className='main'>
+        <div className='left'>  
+          <Typography  variant='h4' >General Details</Typography>
+          <br></br>
+          <Textbox id="name" value={values.name} name="Fullname"></Textbox>
+          <br></br>
+          <Bradio id="gender" name='Gender' value={values.gender}></Bradio>
+          <br></br>
+          <DropdownM id="marital" val='MaritalStatus' value={values.marital} name='Marital Status'></DropdownM>
+          <br></br>
+          <DropdownC id="caste" val='CasteList' value={values.caste} name='Caste'></DropdownC>
+          <br></br>
+          <DropdownR id="religion" value={values.religion} name='Religion'></DropdownR>
+        </div>
+        <div className='right'>
+          <img width='100px' src='schemeup.jpeg'></img>
+          <DropdownEdu id="eduqual" value={values.eduqual} name='Highest Educational Qualification'></DropdownEdu>
+          <DropdownEmp id="emp" value={values.emp} name='Employment Status'></DropdownEmp>
+          <Numbox id="income" value={values.income} name='Annual Income' val='₹'></Numbox>
+          <Uibutton id="submit" name='Submit'></Uibutton>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default App;
